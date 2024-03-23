f27c57a0a98f7c3a25a89f375860224a1312a268
wzy236 <wzy2365@yahoo.com>
2024-03-22 20:29:29 -0700

    fix the bug in checkUniqueFunderaise function

--- contracts/TrustableFund.sol
+++ contracts/TrustableFund.sol
@@ -22,8 +22,8 @@ contract TrustableFund {
         address fundraiser;
     }
 
-    Fundraise[] fundingList;
-    DonationHistory [] totalDonationList;
+    Fundraise[] fundList;
+    DonationHistory [] donationList;
 
     // User address to user related transaction information
     mapping (address => uint[]) donationRecords;
@@ -39,9 +39,9 @@ contract TrustableFund {
     modifier checkUniqueFunderaise() {
         //check if user only have one active funderaise
         uint length=fundRecords[msg.sender].length;
-        uint latestID=donationRecords[msg.sender][length-1];
+        uint latestID=fundRecords[msg.sender][length-1];
 
-        require(fundingList[latestID].active == false,"You all ready have openning Fundraise");
+        require(fundList[latestID].active == false,"You all ready have openning Fundraise");
             _;
     }
 
@@ -52,12 +52,12 @@ contract TrustableFund {
             string memory _imageurl,
             uint _deadline
         ) public checkUniqueFunderaise(){
-        // Create a new Fundraise struct and push it to the fundingList
-        uint fundID = fundingList.length;
+        // Create a new Fundraise struct and push it to the fundList
+        uint fundID = fundList.length;
         uint[] memory _donationList;
-        fundingList.push(Fundraise({
+        fundList.push(Fundraise({
             ownerAddress: msg.sender, // The address of the user who is creating the fundraise
-            fundID: fundID, // Unique ID based on the length of the fundingList
+            fundID: fundID, // Unique ID based on the length of the fundList
             goal: _goal,
             donation: 0, // Initial donation is 0
             donationList: _donationList, // Initialize an empty array
@@ -77,28 +77,28 @@ contract TrustableFund {
             uint _fundID,
             string memory _time
         ) public payable{
-        // Create a new Fundraise struct and push it to the fundingList
-        require(fundingList[_fundID].active==true, "fundraise is not active");
+        // Create a new Fundraise struct and push it to the fundList
+        require(fundList[_fundID].active==true, "fundraise is not active");
 
         // get the current total DonationList
-        uint transID=totalDonationList.length;
+        uint transID=donationList.length;
         // get the current total donation history in total DonationList
-        totalDonationList.push(DonationHistory({
+        donationList.push(DonationHistory({
             fundID: _fundID,
             time: _time,
             transAmount: msg.value, // the donation amount
             donator: msg.sender, 
-            fundraiser: fundingList[_fundID].ownerAddress
+            fundraiser: fundList[_fundID].ownerAddress
         }));
-        fundingList[_fundID].donation+= msg.value;
-        fundingList[_fundID].donationList.push(transID);
+        fundList[_fundID].donation+= msg.value;
+        fundList[_fundID].donationList.push(transID);
         donationRecords[msg.sender].push(transID);
 
         emit DonateSuccess(_fundID, transID, msg.value);
     }
 
-    function getfundingList() public view returns(Fundraise[] memory, bool) {
-        return (fundingList, true);
+    function getfundList() public view returns(Fundraise[] memory, bool) {
+        return (fundList, true);
     }
 
 
