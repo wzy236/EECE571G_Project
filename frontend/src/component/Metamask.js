a3dec6ad2996cecc8abdf27844df05c421314979
yule <305172302@qq.com>
2024-04-04 23:32:28 -0700

    NavBar & MetaMaskAuth

--- frontend/src/component/Metamask.js
+++ frontend/src/component/Metamask.js
@@ -1,121 +1,138 @@
 import React, { useEffect, useState } from "react";
-import { AppBar, Toolbar, Typography, Button, IconButton, Container, Box } from '@mui/material';
-import { injected } from '../utils/connectors';
-import TrustableFundArtifact from '../contracts/TrustableFund.sol/TrustableFund.json'
-import { useWeb3React } from '@web3-react/core';
-import { Contract, ethers, Signer } from 'ethers';
+import { Button, Alert, Snackbar, Box } from "@mui/material";
+import CheckIcon from "@mui/icons-material/Check";
+import { injected } from "../utils/connectors";
+import TrustableFundArtifact from "../contracts/TrustableFund.sol/TrustableFund.json";
+import { useWeb3React } from "@web3-react/core";
+import { Contract, ethers, Signer, upgrades } from "ethers";
 
 // import Web3ABI from "../../pages/Web3"
 // let w3 = new Web3ABI();
 
 export default function MetaMaskAuth() {
-    const [userAddress, setUserAddress] = useState("");
-    const [showModal, setShowModal] = useState(false)
+  const [userAddress, setUserAddress] = useState("");
+  const [showModal, setShowModal] = useState(false);
 
-    const { active, account, library, activate, deactivate } = useWeb3React()
-    const [signer, setSigner] = useState();
-    const [trustableFundcontract, setTrustableFundContract] = useState();
-    const [trustableFundcontractAddr, setTrustableFundContractAddr] = useState('');
+  const { active, account, library, activate, deactivate } = useWeb3React();
+  const [signer, setSigner] = useState();
+  const [trustableFundcontract, setTrustableFundContract] = useState();
+  const [trustableFundcontractAddr, setTrustableFundContractAddr] =
+    useState("");
+  const [connectAlert, setConnectAlert] = useState(false);
+  const [disconnectAlert, setDisconnectAlert] = useState(false);
 
-    
+  useEffect(() => {
+    if (!library) {
+      setSigner(undefined);
+      return;
+    }
 
+    setSigner(library.getSigner());
+  }, [library]);
 
-    useEffect(()=> {
-        if (!library) {
-          setSigner(undefined);
-          return;
-        }
-    
-        setSigner(library.getSigner());
-      }, [library]);
+  function handleDeployContract(event) {
+    if (!signer) {
+      window.alert("no signer");
+      return;
+    }
 
+    async function deployTrustableFundContract() {
+      const TrustableFund = new ethers.ContractFactory(
+        TrustableFundArtifact.abi,
+        TrustableFundArtifact.bytecode,
+        signer
+      );
 
-    
+      try {
+        const TrustableFundContract = await TrustableFund.deploy();
 
-      function handleDeployContract(event) {
-    
-        if (!signer) {
-          window.alert("no signer")
-          return;
-        }
-    
-        async function deployTrustableFundContract() {
+        const address = await TrustableFundContract.getAddress();
 
+        window.alert(`TrustableFund deployed to: ${address}`);
 
-          const TrustableFund = new ethers.ContractFactory(
-            TrustableFundArtifact.abi,
-            TrustableFundArtifact.bytecode,
-            signer
-          );
-    
-          try {
-
-
-            const TrustableFundContract = await TrustableFund.deploy();
-    
-            const address=await TrustableFundContract.getAddress();
-    
-            
-
-            window.alert(`TrustableFund deployed to: ${address}`);
-    
-            setTrustableFundContractAddr(address);
-          } catch (error) {
-            window.alert(
-              'Error!' + (error && error.message ? `\n\n${error.message}` : '')
-            );
-          }
-        }
-    
-        deployTrustableFundContract(signer);
+        setTrustableFundContractAddr(address);
+      } catch (error) {
+        window.alert(
+          "Error!" + (error && error.message ? `\n\n${error.message}` : "")
+        );
       }
-    
+    }
 
+    deployTrustableFundContract(signer);
+  }
 
-    async function disconnect() {
-        try {
-          deactivate()
-          localStorage.setItem('isWalletConnected', false)
-        } catch (ex) {
-          console.log(ex)
-        }
-      }
+  async function disconnect() {
+    try {
+      deactivate();
+      localStorage.setItem("isWalletConnected", false);
+      setDisconnectAlert(true);
+      console.log(disconnectAlert);
+    } catch (ex) {
+      console.log(ex);
+    }
+  }
 
+  async function connect() {
+    try {
+      await activate(injected);
+      localStorage.setItem("isWalletConnected", true);
+      setConnectAlert(true);
+    } catch (ex) {
+      console.log(ex);
+    }
+  }
 
+  async function handleCloseConnectAlert() {
+    setConnectAlert(false);
+  }
 
- 
+  async function handleCloseDisconnectAlert() {
+    setDisconnectAlert(false);
+  }
 
-    async function connect() {
-        try {
-          await activate(injected)
-          localStorage.setItem('isWalletConnected', true)
-        } catch (ex) {
-          console.log(ex)
-        }
-      }
+  return active ? (
+    <>
+      <Snackbar
+        open={connectAlert}
+        autoHideDuration={5000}
+        onClose={handleCloseConnectAlert}
+      >
+        <Alert
+          onClose={handleCloseConnectAlert}
+          severity="success"
+          variant="filled"
+          sx={{ width: "100%" }}
+        >
+          Successfully connected with{" "}
+          {account.slice(0, 4) + "...." + account.slice(-4)}
+        </Alert>
+      </Snackbar>
+      <Box sx={{ color: "white", display: "flex", flexDirection: "column" }}>
+        <Button sx={{ color: "white" }} onClick={disconnect}>
+          Disconnect
+        </Button>
+      </Box>
+    </>
+  ) : (
+    <>
+      <Snackbar
+        open={disconnectAlert}
+        autoHideDuration={5000}
+        onClose={handleCloseDisconnectAlert}
+      >
+        <Alert
+          onClose={handleCloseDisconnectAlert}
+          severity="success"
+          variant="filled"
+          sx={{ width: "100%" }}
+        >
+          Successfully disconnected with your wallet.
+        </Alert>
+      </Snackbar>
 
-    
-    return (
-        active ? (
-            <>
-                <Box sx={{color:"white", display:"flex", flexDirection:"column"}}>
-                    <Button sx={{color:"white"}} onClick={connect}>
-                        Connected with {account.slice(0,4)+ "...."+ account.slice(-4)}
-                    </Button>
-                    <div>My Account</div>
-
-                    <Button sx={{color:"white"}} onClick={disconnect} >Disconnect</Button>
-
-                    <Button sx={{color:"white"}} onClick={handleDeployContract}  >deployContract</Button>
-                </Box>
-            </>
-        ) : (
-                <Button onClick={connect} sx={{color:"white"}}> Connect Wallet</Button>
-        )
-    );
-
-
-
-
-
+      <Button onClick={connect} sx={{ color: "white" }}>
+        Connect Wallet
+      </Button>
+    </>
+  );
 }
--- frontend/src/component/NavBar.jsx
+++ frontend/src/component/NavBar.jsx
@@ -1,36 +1,128 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-import { AppBar, Toolbar, Typography, Button, IconButton, Container, Box } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
-import MetaMaskAuth from './Metamask';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Box,
+  Tab,
+  Tabs,
+} from "@mui/material";
+import MetaMaskAuth from "./Metamask";
+import { useTheme } from "@emotion/react";
+
+function samePageLinkNavigation(event) {
+  if (
+    event.defaultPrevented ||
+    event.button !== 0 || // ignore everything but left-click
+    event.metaKey ||
+    event.ctrlKey ||
+    event.altKey ||
+    event.shiftKey
+  ) {
+    return false;
+  }
+  return true;
+}
+
+function LinkTab(props) {
+  return (
+    <Tab component={Link} aria-current={props.selected && "page"} {...props} />
+  );
+}
+
+LinkTab.propTypes = {
+  selected: PropTypes.bool,
+};
 
 const NavBar = () => {
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
 
-  
-
+  const handleChange = (event, newValue) => {
+    // event.type can be equal to focus with selectionFollowsFocus.
+    if (
+      event.type !== "click" ||
+      (event.type === "click" && samePageLinkNavigation(event))
+    ) {
+      setValue(newValue);
+    }
+  };
 
   return (
-    <AppBar position="static">
-      <Box sx={{ display: "flex", justifyContent:"space-between", alignItems: 'center', width: "100% ", height:"150px"}}>
-        <Box sx={{ width: "30%", maxWidth:"500px" }}>
-            <img src= "http://www.w3.org/2000/svg" width="16" height="16"
-            alt ="TrustFund"/>
-        </Box >
-
-        <Box sx={{ width: "70%", display: "flex", justifyContent: "flex-end", alignItems: 'center' }}>
-          <Box sx={{ display: "flex", justifyContent:" space-between", alignItems: 'bottom' }}>
-            <Button color="inherit" component={Link} to="/home">Home Page</Button>
-            <Button color="inherit" component={Link} to="/donate">Donate</Button>
-            <Button color="inherit" component={Link} to="/fundraise">Fundraise</Button>
-            <Button color="inherit" component={Link} to="/donatehistory">Donate History</Button>
-            <Button color="inherit" component={Link} to="/fundraisingproject">Fundraising Project</Button>
-            <Button color="inherit"></Button>
+    <Box
+      sx={{
+        flexGrow: 1,
+      }}
+    >
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <img
+            src="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            alt="TrustFund"
+          />
+          <Box flex="1" align="left">
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+            >
+              TrustableFund
+            </Typography>
           </Box>
-          <MetaMaskAuth/>
-        </Box>
-      </Box>
-    </AppBar>
+
+          <Box
+            justifyContent={"flex-end"}
+            spacing={1}
+            sx={{ borderBottom: 1, borderColor: "divider" }}
+          >
+            <Tabs
+              value={value}
+              indicatorColor="primary"
+              textColor="inherit"
+              onChange={handleChange}
+              aria-label="nav tabs example"
+              role="navigation"
+            >
+              <LinkTab label="Home Page" to="/home" />
+              <LinkTab label="Donate" to="/donate" />
+              <LinkTab label="Fundraise" to="/fundraise" />
+              <LinkTab label="Donate History" to="/donatehistory" />
+              <LinkTab label="Fundraising Project" to="/fundraisingproject" />
+            </Tabs>
+          </Box>
+
+          {/* <Button color="inherit" component={Link} to="/home">
+            Home Page
+          </Button>
+          <Divider orientation="vertical" variant="middle" flexItem />
+          <Button color="inherit" component={Link} to="/donate">
+            Donate
+          </Button>
+          <Divider orientation="vertical" variant="middle" flexItem />
+          <Button color="inherit" component={Link} to="/fundraise">
+            Fundraise
+          </Button>
+          <Divider orientation="vertical" variant="middle" flexItem />
+          <Button color="inherit" component={Link} to="/donatehistory">
+            Donate History
+          </Button>
+          <Divider orientation="vertical" variant="middle" flexItem />
+          <Button color="inherit" component={Link} to="/fundraisingproject">
+            Fundraising Project
+          </Button> */}
+
+          <Button color="inherit"></Button>
+
+          <MetaMaskAuth />
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
 
